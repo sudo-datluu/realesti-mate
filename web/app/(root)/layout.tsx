91d954ab2eb7216ddc63a92fc886b5d0e6758339
datluu <tad.louis2810@gmail.com>
2024-11-13 17:03:16 +1100

    fix the layout error

--- web/app/(root)/layout.tsx
+++ web/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSideBar from "@/components/LeftSideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,17 @@ export default function RootLayout({
     <div className="relative flex flex-col">
         <main className="relative flex bg-black-3">
             <LeftSideBar />
-            {children}
+            <section className="flex min-h-screen flex-1 flex-col px-4 sm:px:14">
+              <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                <div className="flex h-16 items-center justify-between md:hidden">  
+                  <Image src="/icons/logo/android-chrome-192x192.png" alt='logo' width={48} height={48}/>
+                  Movile Nav
+                </div>
+                <div className="flex hehe flex-col md:pb-14">
+                  {children} 
+                </div>
+              </div>
+            </section>
             <p className="text-white-1"> Right Sidebar </p>
         </main>
     </div>

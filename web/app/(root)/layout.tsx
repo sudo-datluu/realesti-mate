c698a715f62b1b1a41b22def5e94bf8b0b0a2853
datluu <tad.louis2810@gmail.com>
2024-11-13 17:24:02 +1100

    add right layout

--- web/app/(root)/layout.tsx
+++ web/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSideBar from "@/components/LeftSideBar";
+import RightSideBar from "@/components/RightSideBar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -10,18 +11,18 @@ export default function RootLayout({
     <div className="relative flex flex-col">
         <main className="relative flex bg-black-3">
             <LeftSideBar />
-            <section className="flex min-h-screen flex-1 flex-col px-4 sm:px:14">
+            <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
               <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
                 <div className="flex h-16 items-center justify-between md:hidden">  
                   <Image src="/icons/logo/android-chrome-192x192.png" alt='logo' width={48} height={48}/>
                   Movile Nav
                 </div>
-                <div className="flex hehe flex-col md:pb-14">
+                <div className="flex flex-col md:pb-14">
                   {children} 
                 </div>
               </div>
             </section>
-            <p className="text-white-1"> Right Sidebar </p>
+            <RightSideBar />
         </main>
     </div>
   );

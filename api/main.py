cd90d7b6188d202a19d6583340db69d84b230be5
datluu <tad.louis2810@gmail.com>
2024-11-26 17:02:26 +1100

    fix the model, test the api to return sydney polygons features

--- api/main.py
+++ api/main.py
@@ -1,27 +1,28 @@
 import os
 from typing import Optional, List
 
+from dotenv import load_dotenv
+
 from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response
 from pydantic import ConfigDict, BaseModel, Field, EmailStr
 from pydantic.functional_validators import BeforeValidator
 
-from typing_extensions import Annotated
-
-from bson import ObjectId
 import motor.motor_asyncio
 from pymongo import ReturnDocument
 
 from mongodb.featuremodel import FeatureModel, FeaturesCollection
 
+from constant import au_postcodes
+
+load_dotenv()
 app = FastAPI(
     title="Real Estimate API"
 )
+
 mongoDBClient = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URI"))
 db = mongoDBClient["realestimate"]
 geojson_features = db["au-geojson-features"]
-
-PyObjectedId = Annotated[str, BeforeValidator(str)]
 
 @app.get(
     "/features/au/{area}",
@@ -32,4 +33,12 @@ async def get_features(area: str):
     # Todo implement other city
     if area not in ["sydney"]:
         raise HTTPException(status_code=404, detail="Area not found")
-
+    area_postcodes = au_postcodes[area]
+    features = await geojson_features.find(
+        {
+            "properties.POA_CODE": {
+                 "$in": area_postcodes
+            }
+        }
+    ).to_list(None)
+    return FeaturesCollection(features=features)
--- api/mongodb/featuremodel.py
+++ api/mongodb/featuremodel.py
@@ -1,9 +1,14 @@
 from pydantic import ConfigDict, BaseModel, Field, EmailStr
 from typing import Optional, List
+from bson import ObjectId
+from pydantic.functional_validators import BeforeValidator
+from typing_extensions import Annotated
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class Geometry(BaseModel):
     type: str
-    coordinates: List[List[List[float]]]
+    coordinates: List[List[List[float]]] | List[List[List[List[float]]]]
 
 class Properties(BaseModel):
     POA_CODE: str
@@ -11,7 +16,7 @@ class Properties(BaseModel):
     SQKM: float
 
 class FeatureModel(BaseModel):
-    id: Optional[str] = Field(None, alias="_id")
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     type: str
     geometry: Geometry
     properties: Properties
@@ -20,25 +25,30 @@ class FeatureModel(BaseModel):
         arbitrary_types_allowed=True,
         json_schema_extra={
             "example": {
-                {
-                    "type":"Feature",
-                    "geometry":{
-                        "type":"Polygon",
-                        "coordinates":[
-                            [
-                                [130.85017129599998,-12.45301227060217],[130.849742528,-12.4554665731021],[130.85192067199998,-12.47053443460165],[130.844300896,-12.47371103260155],[130.841802944,-12.467260526601747],[130.833309568,-12.460605910101945],[130.83335568,-12.456840142602058],[130.83766684800003,-12.452448390602187],[130.85017129599998,-12.45301227060217]
-                            ]
-                        ]
-                    },
-                    "properties":{
-                        "POA_CODE":"0800",
-                        "POA_NAME":"0800",
-                        "SQKM":3.12306415134995
-                    }
-                }
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [130.85017129599998, -12.45301227060217],
+                        [130.849742528, -12.4554665731021],
+                        [130.85192067199998, -12.47053443460165],
+                        [130.844300896, -12.47371103260155],
+                        [130.841802944, -12.467260526601747],
+                        [130.833309568, -12.460605910101945],
+                        [130.83335568, -12.456840142602058],
+                        [130.83766684800003, -12.452448390602187],
+                        [130.85017129599998, -12.45301227060217]
+                    ]
+                ]
+            },
+            "properties": {
+                "POA_CODE": "0800",
+                "POA_NAME": "0800",
+                "SQKM": 3.12306415134995
             }
         }
-    )
+    })
 
 class FeaturesCollection(BaseModel):
     """
